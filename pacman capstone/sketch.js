b38a068cb2e1cfad1342e38f39795735b9e1ca23
faraan123 <145151230+faraan123@users.noreply.github.com>
2023-12-15 12:12:58 -0600

    fixxed movement and the made colors and ajusted maze

--- pacman capstone/sketch.js
+++ pacman capstone/sketch.js
@@ -2,12 +2,12 @@
 // Faraan Nawaz
 // Date
 
-
 let maze;
 let ball;
+let ghosts;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(800, 800);
   maze = new Maze();
   ball = new Ball(width / 2, height / 2, 20); // Initialize ball in the middle
 }
@@ -20,18 +20,17 @@ function draw() {
   ball.update();
 }
 
-function keyPressed(){
-  if (keyCode === UP_ARROW){
-    ball.move(0, -1);
-  } else if (keyCode === DOWN_ARROW){
-    ball.move(0, 1);
-  } else if (keyCode === LEFT_ARROW){
-    ball.move(-1, 0);
-  } else if (keyCode === RIGHT_ARROW){
-    ball.move(1, 0);
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    ball.move(0, -2);
+  } else if (keyCode === DOWN_ARROW) {
+    ball.move(0, 2);
+  } else if (keyCode === LEFT_ARROW) {
+    ball.move(-2, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    ball.move(2, 0);
   }
 }
-
 
 class Maze {
   constructor() {
@@ -39,13 +38,13 @@ class Maze {
   }
 
   display() {
-    fill(100);
+    fill(color("blue"));
     noStroke();
 
-    rect(50, 50, 10, 500);
-    rect(60, 50, 500, 10);
-    rect(550, 50, 10, 510);
-    rect(50, 550, 500, 10);
+    rect(50, 50, 10, 600);
+    rect(60, 50, 600, 10);
+    rect(650, 50, 10, 610);
+    rect(50, 650, 600, 10);
   }
 }
 
@@ -54,13 +53,14 @@ class Ball {
     this.x = x;
     this.y = y;
     this.radius = radius;
-    this.speedX = 3; // Initial speed in the x-direction
-    this.speedY = 2; // Initial speed in the y-direction
+    this.speedX = 5; // Initial speed in the x-direction
+    this.speedY = 4; // Initial speed in the y-direction
   }
 
   display() {
-    fill(255);
+    fill(color("yellow"));
     ellipse(this.x, this.y, this.radius * 2);
+    
   }
 
   update() {
@@ -69,13 +69,19 @@ class Ball {
     this.y += this.speedY;
 
     // Check and handle collisions with walls
-    if (this.x - this.radius < 60 || this.x + this.radius > 550) {
-      this.speedX *= -1; // Reverse direction in x-axis
+    if (this.x - this.radius < 60 || this.x + this.radius > 650) {
+      this.speedX *= 0; // Reverse direction in x-axis
     }
 
-    if (this.y - this.radius < 60 || this.y + this.radius > 550) {
-      this.speedY *= -1; // Reverse direction in y-axis
+    if (this.y - this.radius < 60 || this.y + this.radius > 650) {
+      this.speedY *= 0; // Reverse direction in y-axis
     }
+  }
+
+  move(x, y) {
+    // Move the ball by a certain amount
+    this.speedX= x;
+    this.speedY= y;
   }
 }
 
